e64549ac56528794681af2880b27c51eb8c794f6
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2016-10-22 16:56:47 -0400

    Replace SimpleHTTPServer with Flask Server to handle POST Requests

--- server.py
+++ server.py
@@ -1,74 +1,13 @@
-from http.server import SimpleHTTPRequestHandler, HTTPServer
-# from fetch import query
-from urllib.parse import urlparse, parse_qs
+from flask import Flask, request, jsonify
+app = Flask(__name__)
 
-class HTTPHandler(SimpleHTTPRequestHandler):
+@app.route('/')
+def hello_world():
+    return 'Hello World'
 
-    def do_GET(self):
-        print(self.path)
-        params = parse_qs(urlparse(self.path).query)
-        # params = {'lat' : [1,2], 'lng': [3,4]}
-        print(params, self.path)
-        try:
-            res = 4 #query(float(params['lat'][0]), float(params['lng'][0]))
-            self.send_response(200)
-            self.send_header('Content-type','text/html')
-            self.end_headers()
-            self.wfile.write(bytes(str(res), "utf8"))
-        except (ValueError, KeyError) as e:
-            self.send_response(400)
-            self.send_header('Content-type','text/html')
-            self.end_headers()
-            self.wfile.write(bytes("Bad request params", "utf8"))
-        return
+@app.route('/get_image', methods=['POST'])
+def get_image():
+    image = request.form['image']
+    return image
 
-def run():
-  print('starting server...')
-  server_address = ('', 8081)
-  httpd = HTTPServer(server_address, HTTPHandler)
-  httpd.serve_forever()
-
-run()
-
-
-# from http.server import SimpleHTTPRequestHandler, HTTPServer
-# # from fetch import query
-# from urllib.parse import urlparse, parse_qs
-
-# class HTTPHandler(SimpleHTTPRequestHandler):
-#     def do_GET(self):
-#         print(self.path)
-#         print(urlparse(self.path))
-#         try:
-#             self.send_response(200)
-#             self.send_header('Content-type','text/html')
-#             self.end_headers()
-#         except:
-#             self.send_response(400)
-#             self.send_header('Content-type','text/html')
-#             self.end_headers()
-#         return
-#         # params = parse_qs(urlparse(self.path).query)
-#         # print(params, self.path)
-
-#         # try:
-#         #     res = query(float(params['img'][0]))
-#         #     self.send_response(200)
-#         #     self.send_header('Content-type', 'text/html')
-#         #     self.end_headers()
-#         #     self.wfile.write(bytes(str(res), "utf8"))
-#         # except:
-#         #     self.send_response(400)
-#         #     self.send_header('Content-type', 'text/html')
-#         #     self.end_headers()
-#         #     self.wfile.write(bytes("Bad request params", "utf8"))
-#         # return
-
-# def main():
-#     server_address = ('0.0.0.0', 8081)
-#     print('Serving HTTP on ', server_address, ' ...')
-#     httpd = HTTPServer(server_address, HTTPHandler)
-#     httpd.serve_forever()
-
-# if __name__ == "__main__":
-#     main()
+app.run(host='0.0.0.0', debug=True, threaded=True)
