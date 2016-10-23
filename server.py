eea918cf59bb79d08459678ea9b2041ce78884a1
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2016-10-22 21:49:53 -0400

    Modify server file by adding image counter

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, jsonify
+from face_labeler import emojify
 app = Flask(__name__)
+
+_numPics = 0
 
 @app.route('/')
 def hello_world():
@@ -7,7 +10,12 @@ def hello_world():
 
 @app.route('/get_image', methods=['POST'])
 def get_image():
+    global _numPics
     image = request.form['image']
+    # print(image)
+
+    emojified_image = emojify(image, _numPics)
+    _numPics += 1
     return image
 
 app.run(host='0.0.0.0', debug=True, threaded=True)
